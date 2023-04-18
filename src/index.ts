8a14448d7a5e8803678de3ae20ddc704e2372d31
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-04-18 17:16:50 -0500

    adding autogenerated code comments

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,12 @@ const app: express.Application = express();
 // set  port, listen for requests
 const port: number = 3000;
 
+// basic get hello world response
 app.get('/', (req: express.Request, res: express.Response) => {
   res.send(`Hello World!`);
 });
 
+// start server
 app.listen(port, () => {
     console.log(`TypeScript express project listening at http://localhost:${port}`)
 })
