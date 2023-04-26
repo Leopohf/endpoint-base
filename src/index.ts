4c2aaf476f35ab0b53dba95c75a6a5c6e3158e89
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-04-25 22:06:05 -0500

    create example endpoints

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,23 @@ const app: express.Application = express();
 // set  port, listen for requests
 const port: number = 3000;
 
+// catch all request
+app.all('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.log("Entering the server");
+  console.log(req.method);
+  // jumping to the next middleware
+    next();
+});
+
 // basic get hello world response
 app.get('/', (req: express.Request, res: express.Response) => {
+  console.log("Entering to root");
   res.send(`Hello World!`);
+});
+
+app.get('/col', (req: express.Request, res: express.Response) => {
+  console.log("Entering to col");
+  res.send(`Hello from Col`);
 });
 
 // start server
