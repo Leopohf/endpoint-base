80e80455f9991def9e5b6b0e70e78c6f79f34891
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-05-05 17:48:57 -0500

    AI created new endpoints using body instead of parameters

--- src/routes/math/math.routes.config.ts
+++ src/routes/math/math.routes.config.ts
@@ -8,6 +8,30 @@ export class MathRoutesConfig extends CommonRoutesConfig {
     }
 
     configureRoutes(): express.Application {
+        this.app.route('/math/add')
+            .get((req: express.Request, res: express.Response) => {
+                const { a, b } = req.body;
+                res.status(200).send({ result: a + b });
+            });
+
+        this.app.route('/math/subtract')
+            .get((req: express.Request, res: express.Response) => {
+                const { a, b } = req.body;
+                res.status(200).send({ result: a - b });
+            });
+
+        this.app.route('/math/multiply')
+            .get((req: express.Request, res: express.Response) => {
+                const { a, b } = req.body;
+                res.status(200).send({ result: a * b });
+            });
+
+        this.app.route('/math/divide')
+            .get((req: express.Request, res: express.Response) => {
+                const { a, b } = req.body;
+                res.status(200).send({ result: a / b });
+            });
+        
         return this.app;
     }
 
