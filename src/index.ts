7aab76d5742f686bbf597ec6b96ae80a381a0a16
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-05-06 22:14:07 -0500

    updated index application

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import * as https from 'https';
 
 import { CommonRoutesConfig } from './routes/common/common.routes.config';
+import { MathRoutesConfig } from './routes/math/math.routes.config';
 
 // Create a new express app instance
 const app: express.Application = express();
@@ -12,57 +13,9 @@ const port: number = 3000;
 // Create a set of routes
 const routes: Array<CommonRoutesConfig> = [];
 
-// catch all request
-app.all('*', (req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.log("Entering the server");
-  console.log(req.method);
-  // jumping to the next middleware
-    next();
-});
+// adding middleware to parse all incoming requests as JSON
+app.use(express.json());
 
-// endpoint to add two values
-app.get('/add/:a/:b', (req: express.Request, res: express.Response) => {
-  console.log("Entering to add");
-  let ans = Number.parseFloat(req.params.a) + Number.parseFloat(req.params.b);
-  res.send(`${req.params.a} + ${req.params.b} = ${ans}`);
-});
+// adding routes to the app
+routes.push(new MathRoutesConfig(app));
 
-// endpoint to substract two values
-app.get('/sub/:a/:b', (req: express.Request, res: express.Response) => {
-  console.log("Entering to sub");
-  let ans = Number.parseFloat(req.params.a) - Number.parseFloat(req.params.b);
-  res.send(`${req.params.a} - ${req.params.b} = ${ans}`);
-});
-
-// endpoint to multiply two values
-app.get('/mul/:a/:b', (req: express.Request, res: express.Response) => {
-  console.log("Entering to mul");
-  let ans = Number.parseFloat(req.params.a) * Number.parseFloat(req.params.b);
-  res.send(`${req.params.a} * ${req.params.b} = ${ans}`);
-});
-
-// endpoint to divide two values
-app.get('/div/:a/:b', (req: express.Request, res: express.Response) => {
-  console.log("Entering to div");
-  let ans = Number.parseFloat(req.params.a) / Number.parseFloat(req.params.b);
-  res.send(`${req.params.a} / ${req.params.b} = ${ans}`);
-});
-
-// endpoint to calculate square root
-app.get('/sqrt/:a', (req: express.Request, res: express.Response) => {
-  console.log("Entering to sqrt");
-  let ans = Math.sqrt(Number.parseFloat(req.params.a));
-  res.send(`sqrt(${req.params.a}) = ${ans}`);
-});
-
-// endpoint to calculate power
-app.get('/pow/:a/:b', (req: express.Request, res: express.Response) => {
-  console.log("Entering to pow");
-  let ans = Math.pow(Number.parseFloat(req.params.a), Number.parseFloat(req.params.b));
-  res.send(`${req.params.a} ^ ${req.params.b} = ${ans}`);
-});
-
-// start server
-app.listen(port, () => {
-    console.log(`TypeScript express project listening at http://localhost:${port}`)
-})
