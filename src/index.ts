bd3971b4a3acbf36bd520a12fc47517f1b96c283
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-05-06 22:25:59 -0500

    adding server start to watch on the defined endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import * as https from 'https';
+import debug from 'debug';
 
 import { CommonRoutesConfig } from './routes/common/common.routes.config';
 import { MathRoutesConfig } from './routes/math/math.routes.config';
@@ -12,6 +13,8 @@ const server = https.createServer(app);
 const port: number = 3000;
 // Create a set of routes
 const routes: Array<CommonRoutesConfig> = [];
+// Create debug element for the app
+const debugLog: debug.IDebugger = debug('app');
 
 // adding middleware to parse all incoming requests as JSON
 app.use(express.json());
@@ -19,3 +22,9 @@ app.use(express.json());
 // adding routes to the app
 routes.push(new MathRoutesConfig(app));
 
+server.listen(port, () => {
+  routes.forEach((route: CommonRoutesConfig) => {
+    debugLog(`Routes configured for ${route.getName()}`);
+  });
+  console.log(`Server running at http://localhost:${port}`);
+});
