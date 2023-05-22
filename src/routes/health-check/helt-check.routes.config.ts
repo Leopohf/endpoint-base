19bcd6de365e254d5015e904138525c2a2881fac
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-05-22 10:06:30 -0500

    improved routes for math and health

--- src/routes/health-check/helt-check.routes.config.ts
+++ src/routes/health-check/helt-check.routes.config.ts
@@ -2,12 +2,15 @@ import { CommonRoutesConfig } from "../common/common.routes.config";
 import express from "express";
 
 export class HealtCheck extends CommonRoutesConfig {
+
+  private static ROUTE_HEALTH_PREFIX = '/health';
+
   constructor(app: express.Application) {
     super(app, "HealtCheck");
   }
 
   configureRoutes(): express.Application {
-    this.app.route('/health')
+    this.app.route(`${HealtCheck.ROUTE_HEALTH_PREFIX}`)
         .get((req: express.Request, res: express.Response) => {
             res.status(200).send("SUCCESS");
         });
--- src/routes/math/math.routes.config.ts
+++ src/routes/math/math.routes.config.ts
@@ -3,6 +3,8 @@ import express from 'express';
 
 export class MathRoutesConfig extends CommonRoutesConfig {
 
+    private static ROUTE_MATH_PREFIX = '/math';
+
     constructor(app: express.Application) {
         super(app, 'MathRoutes');
     }
@@ -11,25 +13,25 @@ export class MathRoutesConfig extends CommonRoutesConfig {
 
         // route handlers
         // basic add, subtract, multiply, divide operations
-        this.app.route('/math/add')
+        this.app.route(`${MathRoutesConfig.ROUTE_MATH_PREFIX}/add`)
             .get((req: express.Request, res: express.Response) => {
                 const { a, b } = req.body;
                 res.status(200).send({ result: a + b });
             });
 
-        this.app.route('/math/subtract')
+        this.app.route(`${MathRoutesConfig.ROUTE_MATH_PREFIX}/subtract`)
             .get((req: express.Request, res: express.Response) => {
                 const { a, b } = req.body;
                 res.status(200).send({ result: a - b });
             });
 
-        this.app.route('/math/multiply')
+        this.app.route(`${MathRoutesConfig.ROUTE_MATH_PREFIX}/multiply`)
             .get((req: express.Request, res: express.Response) => {
                 const { a, b } = req.body;
                 res.status(200).send({ result: a * b });
             });
 
-        this.app.route('/math/divide')
+        this.app.route(`${MathRoutesConfig.ROUTE_MATH_PREFIX}/divide`)
             .get((req: express.Request, res: express.Response) => {
                 const { a, b } = req.body;
                 res.status(200).send({ result: a / b });
