0b5d6cff459e08ac022447f1d6459a1c43daeed8
Jorge Leonardo Hernandez Fajardo <leopohf@gmail.com>
2023-07-09 11:28:12 -0500

    updated math functions

--- src/math/functions.ts
+++ src/math/functions.ts
@@ -1,5 +1,5 @@
 
-class MathFunctions {
+export class MathFunctions {
 
     static add(a: number, b: number): number {
         return a + b;
@@ -17,31 +17,8 @@ class MathFunctions {
         return a / b;
     }
 
-    static modulo(a: number, b: number): number {
+    static remainder(a: number, b: number): number {
         return a % b;
     }
 
-    static power(a: number, b: number): number {
-        return a ** b;
-    }
-
-    static factorial(a: number): number {
-        let result = 1;
-        for (let i = 1; i <= a; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-    static fibonacci(a: number): number {
-        if (a <= 1) {
-            return a;
-        }
-        return MathFunctions.fibonacci(a - 1) + MathFunctions.fibonacci(a - 2);
-    }
-
 }
-
-export {
-    MathFunctions
-};
